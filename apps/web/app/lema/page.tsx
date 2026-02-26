28d2f7147b00c3dd85b980033616e1c4a41b6018
Heriniaina <ramaheritoheriniaina@.com>
2026-02-26 15:09:39 +0300

    feat: set up folder structure for marketing pages and LEMA platform

---
+++ apps/web/app/lema/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      <h1 className="text-2xl font-extrabold">Welcome to the LEMA page</h1>
+    </main>
+  );
+}

1311a5ba19a5b55219a662c77044789586c84313
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-03-09 15:20:03 +0300

    feat(index.ts): implement database connection with Drizzle ORM

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,4 +1,37 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { sql } from 'drizzle-orm';
+import { Pool } from 'pg';
+import { schema } from '@/db/schema';
 
-const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool(
+    {
+        connectionString : process.env.DATABASE_URL,
+        max: 20,
+        connectionTimeoutMillis: 10000,
+        idleTimeoutMillis :  30000,
+        maxLifetimeSeconds : 60,
+    }
+);
+
+export const db = drizzle({ 
+    client : pool,
+    schema : schema,
+    casing : 'snake_case' 
+});
+
+export { pool };
+
+//deleted later
+async function testConnexion() {
+    setInterval(async () => {
+        try {
+            await db.execute(sql`SELECT 1`);
+            console.log("Requête envoyée...");
+        } catch (error) {
+            console.error("Erreur lors de la requête :", error);
+        }
+    }, 2000);
+};
+
+testConnexion();

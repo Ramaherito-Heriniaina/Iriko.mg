6aad99ce9af1b1d88948c53188472491f7afbd24
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-03-30 17:48:15 +0300

    fix(users): use createUserSchema for POST /users endpoint

--- apps/api/src/api/users/users.route.ts
+++ apps/api/src/api/users/users.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { userController } from "./users.controller";
 import { validate } from "@/common/middlewares";
-import { updateUserSchema } from "./users.validation";
+import { createUserSchema, updateUserSchema } from "./users.validation";
 
 const userRouter: Router = Router();
 
 userRouter.get('/', userController.findAll);
 userRouter.get('/:id', userController.findById);
-userRouter.post('/', validate(updateUserSchema), userController.create);
+userRouter.post('/', validate(createUserSchema), userController.create);
 userRouter.put('/:id', validate(updateUserSchema), userController.update);
 userRouter.delete('/:id', userController.delete);
 

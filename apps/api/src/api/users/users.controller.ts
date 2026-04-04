df9fd5ee42ef72e6f6801b25eab7fe331f7e5cde
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-04-04 14:36:33 +0300

    refactor(users): remove create method

--- apps/api/src/api/users/users.controller.ts
+++ apps/api/src/api/users/users.controller.ts
@@ -42,36 +42,6 @@ export const userController = {
         }
     },
 
-    create: async (req: Request<{}, {}, CreateUserInput>, res: Response) => {
-
-        try {
-
-            const userData: CreateUserInput = req.body;
-
-            const newCreatedUser = await userService.create(userData);
-
-            res.status(StatusCodes.CREATED).json({
-                success: true,
-                data: newCreatedUser,
-                message: "User created successfully"
-            });
-        } catch (error: any) {
-
-            if (error.code === '23505') {
-                return res.status(StatusCodes.CONFLICT).json({
-                    success: false,
-                    error: "Email already exists"
-                })
-            };
-
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                success: false,
-                error: "Error creating user"
-            });
-
-        }
-    },
-
     update: async (req: Request<{ id: string }, {}, UpdateUserInput>, res: Response) => {
         try {
             const updated = await userService.update(req.params.id, req.body);

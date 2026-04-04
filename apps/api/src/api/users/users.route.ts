ab20282a36381f4213e316b8f560a66454fbdf3e
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-04-04 14:35:54 +0300

    refactor(users): apply requireAuth + requireAdmin globally

--- apps/api/src/api/users/users.route.ts
+++ apps/api/src/api/users/users.route.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { userController } from "./users.controller";
 import { validate } from "@/common/middlewares";
 import { createUserSchema, updateUserSchema } from "./users.validation";
+import { requireAdmin, requireAuth } from "../auth/auth.middleware";
 
 const userRouter: Router = Router();
 
+userRouter.use(requireAuth, requireAdmin);
+
 userRouter.get('/', userController.findAll);
 userRouter.get('/:id', userController.findById);
-userRouter.post('/', validate(createUserSchema), userController.create);
 userRouter.put('/:id', validate(updateUserSchema), userController.update);
 userRouter.delete('/:id', userController.delete);
 
